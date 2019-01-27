dc549532d050e5f644d3611fecfb8da18e668a5d
Nishanth Duraiarasu <dnishanth99@hotmail.com>
2019-01-27 02:34:55 -0600

    initializes and restores options menu

---
+++ options.js
@@ -0,0 +1,39 @@
+var newlineButton = document.getElementById("newlineButton");
+var copyButton = document.getElementById("copyButton");
+var selectedColor = 'rgb(' + 0 + ',' + 162 + ',' + 232 + ')';
+var unSelectedColor = "darkgrey";
+var copyByNewLine = true; // default copy separates by newlines
+// separates by spaces if false
+
+// initializes options menu and restores saved choices
+// defaults to Copy With Newlines
+chrome.storage.sync.get({copyByNewLine: true}, function(result) {
+		if (result.copyByNewLine == true) {
+			newlineButton.style.background = selectedColor;	
+			copyButton.style.background = unSelectedColor;
+			copyByNewLine = true;		
+		}
+		else if (result.copyByNewLine == false) {
+			newlineButton.style.background = unSelectedColor;	
+			copyButton.style.background = selectedColor;
+			copyByNewLine = false;
+		}
+	});
+
+newlineButton.onclick = function() {
+	if (!copyByNewLine) {
+		copyByNewLine = true;
+		newlineButton.style.background = selectedColor;
+		copyButton.style.background = unSelectedColor;
+		chrome.storage.sync.set({"copyByNewLine": true});
+	}
+};
+
+copyButton.onclick = function() {
+	if (copyByNewLine) {
+		copyByNewLine = false;
+		copyButton.style.background = selectedColor;
+		newlineButton.style.background = unSelectedColor;
+		chrome.storage.sync.set({"copyByNewLine": false});
+	}
+};
