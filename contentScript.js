10770d3f2d6ec636938041b3a30fb3b3c0eb4430
Nishanth Duraiarasu <dnishanth99@hotmail.com>
2019-06-21 17:33:27 -0400

    Integrated with Rangy highlighter module to fix selection issues

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,21 @@
+// https://stackoverflow.com/questions/43676331/creating-a-css-class-with-a-javascript-object-or-var/43676931
+// creates css class that will be applied to each selection
+var element = document.createElement("style");
+element.innerHTML = ".hiclass {background-color:" + "rgb(51, 144, 255)" + "; color: white}";
+var header = document.getElementsByTagName("HEAD")[0];
+header.appendChild(element);
+
+rangy.init();
+
+var highlighter = rangy.createHighlighter();
+
+highlighter.addClassApplier(rangy.createClassApplier("hiclass", {
+	elementTagName: "span",
+	ignoreWhiteSpace: true,
+	tagNames: ["span", "a"],
+	elementProperties: {id: "HIGHLIGHT"}
+}));
+
 // stores all currently highlighted nodes
 var highlights = new Array();
 
@@ -23,19 +41,39 @@ var lockSelect = false;
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		var selectedText = new Array();
-		for (var i = 0; i < highlights.length; i++) {
-			if (highlights[i] != null) {
-				selectedText.push(highlights[i].textContent);
+		for (var i = 0; i < highlighter.highlights.length; i++) {
+			if (highlighter.highlights[i].getText() != null || highlighter.highlights[i].getText() != "" 
+				|| highlighter.highlights[i].getText() != " ") {
+				var elementA, elementB;
+				var text = "";
+				for (var i = 0; i < highlighter.highlights.length; i++) {
+					var highlightEls = highlighter.highlights[i].getHighlightElements();
+
+					text += highlightEls[0].innerText;
+					if (highlightEls.length > 1) {
+						for (var x = 0; x < highlightEls.length - 1; x++) {
+							elementA = highlightEls[x].getBoundingClientRect();
+							elementB = highlightEls[x + 1].getBoundingClientRect();
+							if (isElementOnNextLine(elementA, elementB)) {
+								selectedText.push(text);
+								text = "";
+							}
+							text += highlightEls[x + 1].innerText;
+						}
+					}
+					selectedText.push(text);
+					text = "";
+				}
 			}
 		}
-		sendResponse({array: selectedText});
-	});
+			sendResponse({array: selectedText});
+		});
 
 // highlights selected text when mouseup and ctrl down
 // ctrl not needed when selection lock is on
-document.addEventListener("mouseup", function(e) {
-	if ((e.target.nodeName != "HIGHLIGHT") && (lockSelect || ((!isMac && e.ctrlKey) || (isMac && e.metaKey)) )) {
-		highlightText();
+document.addEventListener("mouseup", function(e) { //(e.target.id != "HIGHLIGHT")// nodename != HIGHLIGHT originally
+	if (lockSelect || ((!isMac && e.ctrlKey) || (isMac && e.metaKey)) ) {
+		highlighter.highlightSelection("hiclass");
 	}
 });
 
@@ -44,11 +82,11 @@ document.addEventListener("mouseup", function(e) {
 // disabled when lock selection is on, clear by shift-L instead
 document.addEventListener("mousedown", function(e) {
 	if (lockSelect == false && (((!isMac && !e.ctrlKey) || (isMac && !e.metaKey)) && (e.button == 0))) {
-		removeHighlights();
+		highlighter.removeAllHighlights();
 	}
 
 	if (e.button == 2) {
-		if (e.target.nodeName == "HIGHLIGHT") {
+		if (e.target.id == "HIGHLIGHT") {
 			chrome.runtime.sendMessage({message: "addOptions"});
 		}
 		else {
@@ -60,39 +98,107 @@ document.addEventListener("mousedown", function(e) {
 // overrides copy event by sending selected text to
 // the clipboard
 document.addEventListener('copy', function(e) {
-	if (highlights.length != 0) {
-		e.clipboardData.setData('text/plain', getSelectedText());
+	//
+	var text = "";
+	for (var i = 0; i < highlighter.highlights.length; i++) {
+		var highlightEls = highlighter.highlights[i].getHighlightElements();
+		text = text + "" + i + ". " + highlightEls.length + "\n";
+		//text += highlighter.highlights[i].toString();
+		for (var x = 0; x < highlightEls.length; x++) {
+			text += highlightEls[x].innerText;// + "" + highlightEls[x].getRange();
+			text += "\n";
+			var rect = highlightEls[x].getBoundingClientRect();
+			text += "T:" + rect.top + ", " + "B:" + rect.bottom + ", " + "L:" + rect.left + ", " + "R:" + rect.right + "\n";
+		}
+		text += "\n\n";
+	}
+
+
+
+	alert(text);
+	//
+
+	if (highlighter.highlights.length != 0) {
+		e.clipboardData.setData('text/plain', extractSelectionText());
 		e.preventDefault(); // prevents default copy event
 	}
 });
 
+function extractSelectionText() {
+	var elementA, elementB;
+	var text = "";
+	for (var i = 0; i < highlighter.highlights.length; i++) {
+		var highlightEls = highlighter.highlights[i].getHighlightElements();
+		if (copyByBullet) {
+			text += "• ";
+		}
+		text += highlightEls[0].innerText;
+		if (highlightEls.length > 1) {
+			for (var x = 0; x < highlightEls.length - 1; x++) {
+				elementA = highlightEls[x].getBoundingClientRect();
+				elementB = highlightEls[x + 1].getBoundingClientRect();
+				if (isElementOnNextLine(elementA, elementB)) {
+					text += "\n";
+					if (copyByBullet) {
+						text += "• ";
+					}
+				}
+				text += highlightEls[x + 1].innerText;
+			}
+		}
+
+		if (i != highlighter.highlights.length - 1) {
+			if (copyByNewLine || copyByBullet) {
+				text += "\n";
+			}
+			else {
+				text += " ";
+			}
+		}
+	}
+	return text;
+}
+
+
+function isElementOnNextLine(elA, elB) {
+	// alert("elBtop: " + elB.top + "\nelAbot: " + elA.bottom);
+	// alert(elB.top > elA.bottom);
+
+	return (elB.top > elA.bottom) || (elB.bottom < elA.top);
+	// !(((elB.top >= elA.top) && (elB.top <= elA.bot)) || ((elB.bot >= elA.top) && (elB.bot <= elA.bot)));
+}
+
+
 // removes the most recent selection when ctrl + z is pressed
 document.addEventListener('keydown', function(e) {
-	if (highlights.length != 0 && ((!isMac && e.ctrlKey) || (isMac && e.metaKey)) && e.key == "z") {
-		var lastEntry = highlights[highlights.length - 1];
-		// stores id's of child selections to be removed
-		var removeArray = new Array();
-		//stores underlying highlights of the last selection
-		var childElements;
-		// removes underneath selections if they exist  
-		if (lastEntry != null) {
-			childElements = lastEntry.children;
-			for (var i = 0; i < childElements.length; i++) {
-				removeArray.push(childElements[i].id);
-			}
-			for (var i = 0; i < removeArray.length; i++) {
-				removeHighlight(removeArray[i]);
-			}
-		}
-		// removes most recent selection
-		for (var i = highlights.length - 1; i >= 0; i--) {
-			if (highlights[i] != null) {
-				removeHighlight(i);
-				highlights.pop();
-				break;
-			}
-			highlights.pop();
-		}
+	if (highlighter.highlights.length != 0 && ((!isMac && e.ctrlKey) || (isMac && e.metaKey)) && e.key == "z") {
+		highlighter.removeHighlights([highlighter.highlights[highlighter.highlights.length-1]]);
+
+
+		// var lastEntry = highlights[highlights.length - 1];
+		// // stores id's of child selections to be removed
+		// var removeArray = new Array();
+		// //stores underlying highlights of the last selection
+		// var childElements;
+		// // removes underneath selections if they exist  
+		// if (lastEntry != null) {
+		// 	childElements = lastEntry.children;
+		// 	for (var i = 0; i < childElements.length; i++) {
+		// 		removeArray.push(childElements[i].id);
+		// 	}
+		// 	for (var i = 0; i < removeArray.length; i++) {
+		// 		removeHighlight(removeArray[i]);
+		// 	}
+		// }
+		// // removes most recent selection
+		// for (var i = highlights.length - 1; i >= 0; i--) {
+		// 	if (highlights[i] != null) {
+		// 		removeHighlight(i);
+		// 		highlights.pop();
+		// 		break;
+		// 	}
+		// 	highlights.pop();
+		// }
 	}
 });
 
@@ -102,7 +208,7 @@ document.addEventListener('keydown', function(e) {
 document.addEventListener('keydown', function(e) {
 	if (e.shiftKey && ((!isMac && e.ctrlKey) || (isMac && e.metaKey)) && e.keyCode == 76) {
 		if (lockSelect) {
-			removeHighlights();
+			highlighter.removeAllHighlights();
 		}
 		lockSelect = !lockSelect;
 	}
