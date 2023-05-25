89864c0377ea511ec5ca8ef22e4e8e7ba115fc29
Nishanth Duraiarasu <dnishanth99@gmail.com>
2023-05-25 00:37:04 -0400

    Code cleanup + arrow functions

--- contentScript.js
+++ contentScript.js
@@ -3,8 +3,17 @@ function isMacOS() {
 	return navigator.platform.indexOf('Mac') > -1;
 }
 
-// used to replace ctrl with cmd when using a mac
-var isMac = isMacOS();
+// Initialize
+var copyByNewLine = true; // add newlines after selections if true
+var copyBySpaces = false; // add spaces after selections if true
+var copyByBullet = false; // add a bullet before selections if true
+chrome.storage.sync.get({copyByNewLine: true, copyBySpaces: false,
+    copyByBullet: false}, result => {
+    var {copyByNewLine, copyBySpaces, copyByBullet} = result;
+});
+
+var lockSelect = false; // allow selection without holding ctrl if true
+const isMac = isMacOS(); // used to replace ctrl with cmd when using a mac
 var bgColor = "rgb(51, 144, 255)"; // windows blue
 var textColor = "; color: white}"; // windows white
 
@@ -13,38 +22,23 @@ if (isMac) {
 	textColor = "}"; // mac no text color
 }
 
-
 // https://stackoverflow.com/questions/43676331/creating-a-css-class-with-a-javascript-object-or-var/43676931
 // creates css class that will be applied to each selection with rangy
 var element = document.createElement("style");
-element.innerHTML = ".hiclass {background-color:" + bgColor + textColor;
+const HIGHLIGHT_CLASS = "hiclass";
+const HIGHLIGHT_ID = "HIGHLIGHT";
+element.innerHTML = `.${HIGHLIGHT_CLASS} {background-color: ${bgColor}${textColor}`;
 var header = document.getElementsByTagName("HEAD")[0];
 header.appendChild(element);
 
 rangy.init();
 var highlighter = rangy.createHighlighter();
-
-highlighter.addClassApplier(rangy.createClassApplier("hiclass", {
+highlighter.addClassApplier(rangy.createClassApplier(HIGHLIGHT_CLASS, {
 	elementTagName: "span",
 	ignoreWhiteSpace: true,
 	tagNames: ["span", "a"],
-	elementProperties: {id: "HIGHLIGHT", draggable: "true"}
+	elementProperties: {id: HIGHLIGHT_ID, draggable: "true"}
 }));
-
-// adds newlines after selections if true
-var copyByNewLine = true;
-// adds spaces after selections if true
-var copyBySpaces = false;
-// adds a bullet before selections if true
-var copyByBullet = false;
-
-chrome.storage.sync.get({copyByNewLine: true, copyBySpaces: false,
-    copyByBullet: false}, result => {
-    var {copyByNewLine, copyBySpaces, copyByBullet} = result;
-});
-
-// allows selection without holding ctrl
-var lockSelect = false;
 
 // Sends array of selected text to the background page
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
@@ -54,63 +48,65 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 });
 
 // when a highlight element is dragged, set the drag text to the selection
-document.addEventListener('dragstart', function(e) {
-	if (e.target.id == "HIGHLIGHT") {
-		dragData = highlighter.getHighlightForElement(e.target).getText()
-		e.dataTransfer.setData('text/plain', dragData)
+document.addEventListener('dragstart', e => {
+	if (e.target.id == HIGHLIGHT_ID) {
+		dragData = highlighter.getHighlightForElement(e.target).getText();
+		e.dataTransfer.setData('text/plain', dragData);
 	}
-	else if (e.target.parentNode.id == "HIGHLIGHT") {
-		dragData = highlighter.getHighlightForElement(e.target.parentNode).getText()
-		e.dataTransfer.setData('text/plain', dragData)
+	else if (e.target.parentNode.id == HIGHLIGHT_ID) { // get parent if normal selection is active
+		dragData = highlighter.getHighlightForElement(e.target.parentNode).getText();
+		e.dataTransfer.setData('text/plain', dragData);
 	}
 });
 
+// Clear selected text
+function clearSelection() {
+    const selection = window.getSelection();
+    if (selection) {
+        selection.empty();
+    }
+}
+
+// Return true if ctrl or cmd is pressed for Windows/Mac
+function selectKeyDown(e) {
+    return (!isMac && e.ctrlKey) || (isMac && e.metaKey);
+}
+
 // highlights selected text when mouseup and ctrl down
 // ctrl not needed when selection lock is on
-document.addEventListener("mouseup", function(e) {
-	// if (lockSelect || ((!isMac && !e.ctrlKey) || (isMac && !e.metaKey)) && highlighter.highlights.length == 0 ) {
-	// 	// serializedSelections = rangy.serializeSelection();
-	// 	// document.getSelection().removeAllRanges();
-	// }
-	if (lockSelect || ((!isMac && e.ctrlKey) || (isMac && e.metaKey)) ) {
-		highlighter.highlightSelection("hiclass");
+document.addEventListener("mouseup", e => {
+	if (lockSelect || selectKeyDown(e)) {
+		highlighter.highlightSelection(HIGHLIGHT_CLASS);
 	}
 });
 
 // clears all selected text when left mousedown and ctrl up
 // and adds options to context menu when a selection is clicked
 // disabled when lock selection is on, clear by ctrl+shift+L instead
-document.addEventListener("mousedown", function(e) {
-	if (lockSelect == false) {
-		if (((!isMac && e.ctrlKey) || (isMac && e.metaKey)) && (e.button == 0) && highlighter.highlights.length == 0) {
-			// try {
-			// 	rangy.deserializeSelection(serializedSelections);
-			// }
-			// catch(err) {
-			// 	serializedSelections = ""
-			// }
-			if (window.getSelection() != "") {
-				highlighter.highlightSelection("hiclass");
-			}
-		}
-		else if (((!isMac && !e.ctrlKey) || (isMac && !e.metaKey)) && (e.button == 0) && (highlighter.highlights.length > 0) && (e.target.id != "HIGHLIGHT")) {
-			// prevents outer element from being highlighted when there is a single highlight
-			var currSelection = window.getSelection();
-			if (currSelection != "") {
-				currSelection.empty();
-			}
-			highlighter.removeAllHighlights();
-		}
-	}
-
-	if (e.button == 2) {
-		if (e.target.id == "HIGHLIGHT") {
+document.addEventListener("mousedown", e => {
+    const LEFT_CLICK = 0;
+    const RIGHT_CLICK = 2;
+    const selectKey = selectKeyDown(e);
+    if (e.button == RIGHT_CLICK) {
+		if (e.target.id == HIGHLIGHT_ID) {
 			chrome.runtime.sendMessage("addOptions");
 		}
 		else {
 			chrome.runtime.sendMessage("removeOptions");
 		}
 	}
+    if (lockSelect) {
+        return;
+    }
+    if (selectKey && e.button == LEFT_CLICK && highlighter.highlights.length == 0
+        && window.getSelection() != "") {
+        highlighter.highlightSelection(HIGHLIGHT_CLASS);
+    }
+    else if (!selectKey && e.button == LEFT_CLICK && highlighter.highlights.length > 0
+        && e.target.id != HIGHLIGHT_ID) {
+        clearSelection();
+        highlighter.removeAllHighlights();
+    }
 });
 
 // Formats selection using copy settings and copies to clipboard
@@ -163,14 +159,10 @@ function isElementOnNextLine(elA, elB) {
 }
 
 // removes the most recent selection when ctrl + z is pressed
-document.addEventListener('keydown', function(e) {
+document.addEventListener('keydown', e => {
 	var highlights = highlighter.highlights;
-	if (highlights.length != 0 && ((!isMac && e.ctrlKey)
-		|| (isMac && e.metaKey)) && e.key == "z") {
-		var currSelection = window.getSelection();
-		if (currSelection) {
-			currSelection.empty();
-		}
+	if (highlights.length != 0 && selectKeyDown(e) && e.key == "z") {
+		clearSelection();
 		highlighter.removeHighlights([highlights[highlights.length-1]]);
 	}
 });
@@ -178,14 +170,10 @@ document.addEventListener('keydown', function(e) {
 // toggles selection lock so that ctrl isn't needed to select
 // selections can only be removed by toggling again
 // 76 = L
-document.addEventListener('keydown', function(e) {
-	if (e.shiftKey && ((!isMac && e.ctrlKey) || (isMac && e.metaKey))
-		&& e.keyCode == 76) {
+document.addEventListener('keydown', e => {
+	if (selectKeyDown(e) && e.shiftKey && e.keyCode == 76) {
 		if (lockSelect) {
-			var currSelection = window.getSelection();
-			if (currSelection != "") {
-				currSelection.empty();
-			}
+			clearSelection();
 			highlighter.removeAllHighlights();
 		}
 		lockSelect = !lockSelect;
@@ -193,7 +181,7 @@ document.addEventListener('keydown', function(e) {
 });
 
 // updates copy option
-chrome.storage.onChanged.addListener(function(changes, areaName) {
+chrome.storage.onChanged.addListener((changes, areaName) => {
 	for (var key in changes) {
 		window[key] = changes[key].newValue;
 	 }
